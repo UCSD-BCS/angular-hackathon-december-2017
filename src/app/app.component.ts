0999f69d5b3fb1016025fba554dba686c09772b5
nixallover <nicole.oliver.42@gmail.com>
2017-12-18 17:48:14 -0800

    created employee model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Employee } from './employee/employee.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Jubilant Octo Waddle';
+  currentEmployee: Employee;
+
+  ngOnInit() {
+    this.currentEmployee = new Employee(123, 'Frank', 'Castle', 'fcastle@ucsd.edu');
+  }
 }
---
+++ src/app/employee/employee.model.ts
@@ -0,0 +1,8 @@
+export class Employee {
+  constructor(
+    public employeeNumber: number,
+    public lastName: string,
+    public firstName: string,
+    public email: string
+  ) {}
+}
